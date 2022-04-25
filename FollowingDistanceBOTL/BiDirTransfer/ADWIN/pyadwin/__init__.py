8268f98a704f39a06f3d2b3347337c3c01adb992
Helen <h.mckay@warwick.ac.uk>
2022-04-25 14:46:33 +0100

    deleted old versions of BOTL

--- FollowingDistanceBOTL/BiDirTransfer/ADWIN/pyadwin/__init__.py
+++
@@ -1,4 +0,0 @@
-# ADWIN code from: https://github.com/rsdevigo/pyAdwin by Rodrigo Sanches Devigo
-from pyadwin.ad_win_list_node import AdWinListNode
-from pyadwin.ad_win_list import AdWinList
-from pyadwin.adwin import Adwin
